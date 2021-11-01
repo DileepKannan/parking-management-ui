bb826a10983f2f0fd545d0edfa8032d540720b5e
Dileep Kannan <ckdileep05@gmail.com>
2021-11-01 12:57:33 +0530

    cc

--- parking-management/src/main/java/com/elseiver/parking/ParkingManagementApplication.java
+++ parking-management/src/main/java/com/elseiver/parking/ParkingManagementApplication.java
@@ -2,8 +2,9 @@ package com.elseiver.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class ParkingManagementApplication {
 
 	public static void main(String[] args) {
