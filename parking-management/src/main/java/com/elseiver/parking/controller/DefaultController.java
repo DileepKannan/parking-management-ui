48ba9834547d2d7630cbcb358a1025a4273ebd46
Dileep Kannan <ckdileep05@gmail.com>
2021-11-01 13:05:28 +0530

    vbfgbngfgf

--- parking-management/src/main/java/com/elseiver/parking/controller/DefaultController.java
+++ parking-management/src/main/java/com/elseiver/parking/controller/DefaultController.java
@@ -10,32 +10,32 @@ public class DefaultController {
 
 	@GetMapping("/")
 	public String home1() {
-		return "/booking";
+		return "booking";
 	}
 
 	@GetMapping("/booking")
 	public String booking() {
-		return "/booking";
+		return "booking";
 	}
 
 	@GetMapping("/closing")
 	public String closing() {
-		return "/closing";
+		return "closing";
 	}
 
 	@GetMapping("/admin")
 	public String admin() {
-		return "/admin";
+		return "admin";
 	}
 
 	@GetMapping("/user")
 	public String user() {
-		return "/user";
+		return "user";
 	}
 
 	@GetMapping("/about")
 	public String about() {
-		return "/about";
+		return "about";
 	}
 
 	@GetMapping("/login")
